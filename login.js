5e55ae23c419fb6b364b42c58df130b88fdd2ac3
Rafael <mesquitarafael053@gmail.com>
2026-05-29 20:39:50 -0300

    Update login.js

--- login.js
+++ login.js
@@ -5,19 +5,18 @@ const btnEye = document.getElementById('btn-eye');
 const formLogin = document.getElementById('form-login');
 
 //função 1: mostrar ou ocultar senha
-btnEye.addEventListener('click', function ()){
+btnEye.addEventListener('click', function () {
     //se o tipo de input for 'password', muda para 'text' para mostrar a senha
     if (passwordInput.type === 'password') {
-        {
             passwordInput.type = 'text';
             //mudar o icone do olho para indicar que a senha está visível
             btnEye.classList.remove('fa-eye');
             btnEye.classList.add('fa-eye-slash');
-        }else {
+        } else {
             //se o tipo do input for 'text', muda para 'password' para ocultar a senha
             passwordInput.type = 'password';
             //mudar o icone do olho para indicar que a senha está oculta
             btnEye.classList.remove('fa-eye-slash');
             btnEye.classList.add('fa-eye');
         }
-    }
+    });
